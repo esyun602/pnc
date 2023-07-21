902aaecd114aece677b5f55a737e5fda3c6c5556
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2023-07-21 20:53:56 +0900

    PlayerMove : 방향키 움직임, 2단 점프

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float movePower = 1f;
+	public float jumpPower = 1f;
+
+	private Rigidbody2D rig;
+	private Vector3 movement;
+	private bool isJumping = false;
+    private int jumpCount = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+			isJumping = true;
+		}
+    }
+    void FixedUpdate()
+	{
+		Move();
+		Jump();
+	}
+
+    private Vector3 moveVelocity;
+	private void Move()
+	{		
+		moveVelocity = Vector3.zero;
+
+		if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+			moveVelocity = Vector3.left;
+		}
+			
+		else if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+			moveVelocity = Vector3.right;
+		}	
+
+		transform.position += moveVelocity * movePower * Time.deltaTime;
+	}
+
+    private Vector2 jumpVelocity;
+	private void Jump()
+	{
+		if (!isJumping || jumpCount <= 0)
+			return;
+
+        // 점프 횟수 제한
+        jumpCount--;
+
+        // 점프 증폭 방지
+		rig.velocity = Vector2.zero;
+
+		jumpVelocity = new Vector2(0, jumpPower);
+		rig.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+		isJumping = false;
+	}
+
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // 땅에 있을 때는 점프 횟수 초기화
+        if(other.gameObject.tag == "Ground")
+        {
+            jumpCount = 2;
+        }
+    }
+}
