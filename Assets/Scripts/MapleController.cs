4576fe2fb70d9286015aaaca8d12fb449e6e8ce1
DESKTOP-CN6KRA6\esyun <tmddbs0602@korea.ac.kr>
2023-07-22 22:16:03 +0900

    단풍 더미 구현, 리소스 수정

---
+++ Assets/Scripts/MapleController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapleController : MonoBehaviour
+{
+    private float mapleMoveAccel = -15f;
+    private const float roundTime = 1f;
+    private float initMapleVelocity => -mapleMoveAccel * roundTime / 2;
+    private float currentMapleVelocity;
+    private float mapleTimePassed = 0;
+
+    private void OnEnable()
+    {
+        mapleTimePassed = 0;
+        currentMapleVelocity = initMapleVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mapleTimePassed += Time.deltaTime;
+        if (mapleTimePassed > roundTime)
+        {
+            mapleTimePassed = 0;
+            mapleMoveAccel *= -1;
+            currentMapleVelocity = initMapleVelocity;
+        }
+        else
+        {
+            currentMapleVelocity += mapleMoveAccel * Time.deltaTime;
+            transform.position += Vector3.right * (currentMapleVelocity * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground")
+        {
+            gameObject.SetActive(false);
+        }
+        else if(collision.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
