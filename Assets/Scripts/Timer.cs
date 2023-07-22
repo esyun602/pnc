e44f14aa8d89bcd9e510557763b873acb14156cf
DESKTOP-CN6KRA6\esyun <tmddbs0602@korea.ac.kr>
2023-07-23 03:01:30 +0900

    단풍 생성 추가

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,8 +6,29 @@ public class Timer : MonoBehaviour
 {
     [SerializeField]
     private Text textBox;
+    [SerializeField]
+    private GameObject maplePrefab;
+
+    private float currentMapleTime = 45f;
+    private const float genHalfWidth = 0.3f;
+
     void Update()
     {
         textBox.text = string.Format("{0:F2}", GameManager.Instance.LeftTime);
+
+        if(GameManager.Instance.LeftTime < currentMapleTime)
+        {
+            var randX = Random.Range(0.5f - genHalfWidth, 0.5f + genHalfWidth);
+            var genPos = Camera.main.ViewportToWorldPoint(new Vector2(randX, 1));
+            genPos.z = 0;
+            var maple = Instantiate(maplePrefab);
+            maple.transform.position = genPos;
+
+            currentMapleTime -= 15f;
+            if(currentMapleTime == 0)
+            {
+                currentMapleTime = -10000;
+            }
+        }
     }
 }
