ebc08157bdc767b9de672811b4b8bcbf21c53824
DESKTOP-CN6KRA6\esyun <tmddbs0602@korea.ac.kr>
2023-07-22 10:13:08 +0900

    시럽 드랍 액션 팔 추가, 요리사 프리팹화

--- Assets/Scripts/HandController/DropHandController.cs
+++ Assets/Scripts/HandController/DropHandController.cs
@@ -8,16 +8,51 @@ public class DropHandController : MonoBehaviour, IHandController
     private GameObject actionSprite;
     [SerializeField]
     private GameObject normalSprite;
+    [SerializeField]
+    private Transform leftArm;
+    [SerializeField]
+    private Transform rightArm;
+
+    [SerializeField]
+    private Transform leftArmLeftMargin;
+    [SerializeField]
+    private Transform leftArmRightMargin;
+    [SerializeField]
+    private Transform rightArmLeftMargin;
+    [SerializeField]
+    private Transform rightArmRightMargin;
 
     void IHandController.UpdatePosition()
     {
+        //TODO: FIX
         if (normalSprite.activeSelf)
         {
-            transform.position = Cursor.Instance.WorldPos;
+            transform.position = new Vector2(Cursor.Instance.WorldPos.x, transform.position.y);
         }
         else
         {
             transform.position = new Vector2(Cursor.Instance.WorldPos.x, transform.position.y);
+        }
+
+        UpdateArm();
+    }
+
+    private void UpdateArm()
+    {
+        var viewPortX = Camera.main.WorldToViewportPoint(transform.position).x;
+        if (viewPortX > 0.5f)
+        {
+            rightArm.gameObject.SetActive(true);
+            leftArm.gameObject.SetActive(false);
+            rightArm.position = Vector3.Lerp(rightArmLeftMargin.position, rightArmRightMargin.position, viewPortX * 2 - 1);
+            rightArm.localRotation = Quaternion.Lerp(rightArmLeftMargin.rotation, rightArmRightMargin.rotation, viewPortX * 2 - 1);
+        }
+        else
+        {
+            leftArm.gameObject.SetActive(true);
+            rightArm.gameObject.SetActive(false);
+            leftArm.position = Vector3.Lerp(leftArmLeftMargin.position, leftArmRightMargin.position, viewPortX * 2);
+            leftArm.localRotation = Quaternion.Lerp(leftArmLeftMargin.rotation, leftArmRightMargin.rotation, viewPortX * 2);
         }
     }
 
